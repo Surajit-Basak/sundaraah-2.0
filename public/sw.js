78a8bc851da13fe37f694dbaa092611464799ae7
Surajit Basak <surajitbasak2023@gmail.com>
2025-07-27 09:46:13 +0000

    whats next?

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,52 @@
+// Basic service worker for caching assets
 
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+const CACHE_NAME = 'sundaraah-showcase-v1';
+const urlsToCache = [
+  '/',
+  '/shop',
+  '/about',
+  '/contact',
+  '/blog',
+  '/login',
+  '/signup',
+  '/manifest.json',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Serve from cache
+        }
+        return fetch(event.request); // Fetch from network
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
